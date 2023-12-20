93b981ccee0e94e13ad789d3576e752f6b2a76f2
Chi-Cheong Weng <chicheongweng@gmail.com>
2023-12-20 07:15:09 +0000

    allow /h2-console path

---
+++ src/main/java/com/zkpytug/com/springbootserver/securityConfig.java
@@ -0,0 +1,16 @@
+package com.zkpytug.com.springbootserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+class securityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        
+        // Reference: https://stackoverflow.com/questions/43794721/spring-boot-h2-console-throws-403-with-spring-security-1-5-2
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+}
