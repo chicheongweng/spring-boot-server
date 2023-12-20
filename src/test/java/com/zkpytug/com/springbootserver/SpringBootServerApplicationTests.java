48cf9049beee00062549d23d16ecc2d3a52cc2f7
Chi-Cheong Weng <chicheongweng@gmail.com>
2023-12-20 03:33:35 +0000

    first commit

---
+++ src/test/java/com/zkpytug/com/springbootserver/SpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkpytug.com.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
